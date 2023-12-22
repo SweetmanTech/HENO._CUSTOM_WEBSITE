a5d85eb71baa9f4cc5dfa791cbac0c506c1aa82a
Paul Martie <paul.martie.tech@gmail.com>
2023-12-22 13:53:36 -0500

    I fix feedback

--- components/Pages/AboutPage/AboutPage.tsx
+++ components/Pages/AboutPage/AboutPage.tsx
@@ -1,6 +1,7 @@
 import useIsMobile from "../../../hooks/useIsMobile"
 import Icon from "../../Core/Icon"
 import Layout from "../../Layout"
+import RecBar from "../../RecBar"
 import SeoHead from "../../SeoHead"
 
 const AboutPage = () => {
@@ -92,20 +93,7 @@ const AboutPage = () => {
               </a>
             </div>
           </div>
-          <div className="absolute top-1 left-1 md:top-2 md:left-2 z-20">
-            <p className="text-[white] text-[10px] md:text-xs">CCTV 002</p>
-          </div>
-          <div className="absolute top-1 right-1 md:top-2 md:right-2 z-20 flex items-center">
-            <div className="w-2 h-2 md:w-3 md:h-3 bg-[red] rounded-full mr-2" />
-            <p className="text-[white] text-[10px] md:text-xs mr-2">REC</p>
-            <Icon name="wifi" raw size={16} color="white" />
-          </div>
-          <div className="absolute bottom-1 left-1 md:bottom-2 md:left-2 z-20">
-            <p className="text-[white] text-[10px] md:text-xs">DUR : 08:13:24:53</p>
-          </div>
-          <div className="absolute bottom-1 right-1 md:bottom-2 md:right-2 z-20">
-            <p className="text-[white] text-[10px] md:text-xs">2024/08/29 15:39:26</p>
-          </div>
+          <RecBar />
         </div>
       </div>
     </Layout>

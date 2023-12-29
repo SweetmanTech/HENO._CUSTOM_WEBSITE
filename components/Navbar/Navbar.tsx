5d1906e105297d9daa1a121be52adeeef6d593ca
Paul Martie <paul.martie.tech@gmail.com>
2023-12-29 14:27:50 -0500

    I complete press page

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,10 +4,9 @@ import { useRouter } from "next/router"
 const Navbar = () => {
   const { pathname } = useRouter()
 
-  const navClasses = `md:min-w-[80px] md:px-[10px] md:h-[40px] 
-  text-[12px] md:text-[16px] md:py-[5px] py-[2px] h-fit uppercase md:capitalize
-  md:border-none bg-darkgray
-  flex items-center justify-center text-[#d2d2d2] font-dresden cursor-pointer`
+  const navClasses = `md:min-w-[80px] px-0 md:px-[10px] md:h-[40px] 
+  text-[11px] md:text-[16px] md:py-[5px] py-[2px] h-fit uppercase md:capitalize
+  md:border-none bg-darkgray flex items-center justify-center text-[#d2d2d2] font-dresden cursor-pointer`
 
   const isHomePage = pathname === "/"
   const isAboutPage = pathname.includes("/about")
@@ -15,7 +14,7 @@ const Navbar = () => {
   const isMintPage = pathname.includes("/mint")
 
   return (
-    <div className="w-full grid grid-cols-5 md:flex gap-x-[10px]">
+    <div className="w-full grid grid-cols-6 md:flex gap-x-[5px] md:gap-x-[10px]">
       <Link href="/">
         <div
           className={`${navClasses}
@@ -46,6 +45,14 @@ const Navbar = () => {
         ${isMintPage ? "border-b-[2px] border-b-[#347fdb] md:!bg-[#347fdb]" : ""}`}
         >
           Web3
+        </div>
+      </Link>
+      <Link href="/press">
+        <div
+          className={`${navClasses}
+        ${isMusicPage ? "border-b-[2px] border-b-[#347fdb] md:!bg-[#347fdb]" : ""}`}
+        >
+          Press
         </div>
       </Link>
       <a href="https://play.mynameisheno.com" target="_blank" rel="noreferrer">

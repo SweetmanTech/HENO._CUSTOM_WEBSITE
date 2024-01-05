6edd2644c31a68c0946d9022093178030bb52bd4
Paul Martie <paul.martie.tech@gmail.com>
2024-01-05 01:08:30 -0500

    I add play relief button on mobile

--- components/Pages/LandingPage/LandingPage.tsx
+++ components/Pages/LandingPage/LandingPage.tsx
@@ -26,17 +26,27 @@ const LandingPage = () => {
   return (
     <Layout type={isMobile ? "mobile" : "base"}>
       <SeoHead title="HENO. HOME" />
-
       <div
         className={`grid grid-cols-1 md:grid-cols-2 gap-y-[10px] md:gap-[20px] 
-      h-full ${!entered ? "hidden" : ""}`}
+        h-[calc(100%-40px)] md:h-full ${!entered ? "hidden" : ""}`}
       >
         <LandingCard title="ABOUT" img="/images/Landing/about.jpg" href="/about" />
         <LandingCard title="MUSIC" img="/images/Landing/music.jpg" href="/music" />
         <LandingCard title="WEB 3" img="/images/Landing/webThree.jpg" href="/web3" />
         <LandingCard title="PRESS" img="/images/Landing/press.jpg" href="/press" />
       </div>
-
+      {isMobile && (
+        <a
+          type="button"
+          className="w-full bg-gray_1 font-dresden h-[35px] mt-[5px]
+        flex justify-center items-center"
+          href="https://play.mynameisheno.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Play Relief
+        </a>
+      )}
       {!entered && <LoadingPage />}
     </Layout>
   )

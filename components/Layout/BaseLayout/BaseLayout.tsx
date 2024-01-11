ecf30dbd65049007c2376b8b6497bdb32e5c892c
SeniorTech <tech.eng.me@gmail.com>
2024-01-11 11:08:39 -0500

    I replace calendar

--- components/Layout/BaseLayout/BaseLayout.tsx
+++ components/Layout/BaseLayout/BaseLayout.tsx
@@ -3,8 +3,6 @@ import { ILayout } from "../types"
 import Navbar from "../../Navbar"
 import CameraFeed from "../../CameraFeed"
 import CameraFeedProvider from "../../../providers/CameraFeedProvider"
-import DateSelect from "../../DateSelect"
-import DateSelectProvider from "../../../providers/DateSelectProvider"
 import { usePageLoad } from "../../../providers/PageLoadProvider"
 
 const BaseLayout = ({ children }: ILayout) => {
@@ -18,15 +16,16 @@ const BaseLayout = ({ children }: ILayout) => {
           <div className="flex-grow w-[calc(100vw-490px)]">{children}</div>
           {entered && (
             <div className="w-[370px] flex flex-col gap-y-[20px]">
-              <div className="border-gray_1 border-[2px] h-[calc(100%-280px)] py-[10px] overflow-y-auto">
+              <div className="border-gray_1 border-[2px] flex-grow py-[10px] overflow-y-auto">
                 <CameraFeedProvider>
                   <CameraFeed />
                 </CameraFeedProvider>
               </div>
               <div className="border-gray_1 border-[2px]">
-                <DateSelectProvider>
-                  <DateSelect />
-                </DateSelectProvider>
+                <a href="https://play.mynameisheno.com" target="_blank" rel="noreferrer">
+                  {/* eslint-disable-next-line  @next/next/no-img-element */}
+                  <img src="/images/Landing/play_relief.png" alt="not found relief" />
+                </a>
               </div>
             </div>
           )}

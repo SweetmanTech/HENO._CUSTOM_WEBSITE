6be9bc291741411b825a06e85c8c424e330afd9d
Sweets Sweetman <sweetmantech@gmail.com>
2024-01-10 23:29:50 -0300

    I update MintButton to include logic for disconnected wallet.

--- components/MintButton/MintButton.tsx
+++ components/MintButton/MintButton.tsx
@@ -1,10 +1,18 @@
+import { useConnectModal } from "@rainbow-me/rainbowkit"
+import { useEthersSigner } from "../../hooks/useEthersSigner"
 import useZoraMint from "../../hooks/useZoraMint"
 import Button from "../Core/Button"
 
 const MintButton = () => {
+  const signer = useEthersSigner()
   const { mintWithRewards } = useZoraMint()
+  const { openConnectModal } = useConnectModal()
 
   const handleClick = async () => {
+    if (!signer) {
+      openConnectModal()
+      return
+    }
     await mintWithRewards()
   }
 

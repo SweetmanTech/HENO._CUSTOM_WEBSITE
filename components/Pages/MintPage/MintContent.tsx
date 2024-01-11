b98439e4b8f6900a1fd3c90234d0a8d0cc035c2c
Sweets Sweetman <sweetmantech@gmail.com>
2024-01-10 23:30:14 -0300

    I remove ConnectButton and make MintButton smarter.

--- components/Pages/MintPage/MintContent.tsx
+++ components/Pages/MintPage/MintContent.tsx
@@ -1,14 +1,9 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit"
 import MintButton from "../../MintButton"
-import { useEthersSigner } from "../../../hooks/useEthersSigner"
 
-const MintContent = () => {
-  const signer = useEthersSigner()
-  return (
-    <div className="p-[10px] border-[2px] border-gray_1 h-full flex items-center justify-center flex-col gap-y-[20px]">
-      {signer ? <MintButton /> : <ConnectButton />}
-    </div>
-  )
-}
+const MintContent = () => (
+  <div className="p-[10px] border-[2px] border-gray_1 h-full flex items-center justify-center flex-col gap-y-[20px] text-white">
+    <MintButton />
+  </div>
+)
 
 export default MintContent

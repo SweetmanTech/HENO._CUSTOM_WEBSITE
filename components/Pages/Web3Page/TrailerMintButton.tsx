5a4277991fc3639afaaef6a5357254eaef62a351
Sweets Sweetman <sweetmantech@gmail.com>
2024-01-21 12:23:30 -0300

    I update to latest version of onchain-magic.

--- components/Pages/Web3Page/TrailerMintButton.tsx
+++ components/Pages/Web3Page/TrailerMintButton.tsx
@@ -1,5 +1,6 @@
 import { useConnectModal } from "@rainbow-me/rainbowkit"
 import { useCollection } from "onchain-magic"
+
 import { useEthersSigner } from "../../../hooks/useEthersSigner"
 import Media from "../../Core/Media"
 import useCheckNetwork from "../../../hooks/useCheckNetwork"
@@ -10,7 +11,14 @@ const TrailerMintButton = ({ isPopup = false }) => {
   const { openConnectModal } = useConnectModal()
   const { checkNetwork } = useCheckNetwork()
   const zoraDropAddress = process.env.NEXT_PUBLIC_DROP_ADDRESS
-  const { collectAll } = useCollection(zoraDropAddress, CHAIN_ID)
+  console.log("SWEETS zoraDropAddress", zoraDropAddress)
+  console.log("SWEETS CHAIN_ID", CHAIN_ID)
+  const { drops, collectAll, priceValues } = useCollection({
+    collectionAddress: zoraDropAddress,
+    chainId: CHAIN_ID,
+    minterOverride: "0xFF8B0f870ff56870Dc5aBd6cB3E6E89c8ba2e062" as any,
+  })
+  console.log("SWEETS drops", drops)
 
   const handleClick = async () => {
     if (!signer) {
@@ -20,6 +28,7 @@ const TrailerMintButton = ({ isPopup = false }) => {
     if (!checkNetwork()) {
       return false
     }
+    console.log("SWEETS priceValues", priceValues)
     const response = await collectAll()
     return response
   }

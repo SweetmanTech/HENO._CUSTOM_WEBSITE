2e059f864893931ba27411cf913e031189b1e1b3
SeniorTech <tech.eng.me@gmail.com>
2024-02-06 10:41:08 -0500

    I fix build error

--- hooks/usePrivyCollect.tsx
+++ hooks/usePrivyCollect.tsx
@@ -1,8 +1,4 @@
-import { useCollection } from "onchain-magic"
-import { BASE_MINTER, CHAIN_ID, IS_TESTNET } from "../lib/consts"
-import usePrivyMulticall from "./usePrivyMulticall"
 import usePreparePrivyWallet from "./usePreparePrivyWallet"
-import _ from "lodash"
 
 const usePrivyCollect = () => {
   const { prepare } = usePreparePrivyWallet()

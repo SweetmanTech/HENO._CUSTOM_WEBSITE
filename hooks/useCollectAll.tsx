05c26d1db7d733bab752086dd85b5e4446b8a6ef
Sweets Sweetman <sweetmantech@gmail.com>
2024-02-06 14:27:13 -0300

    I fix priceValues with latest version of onchain-magic package.

--- hooks/useCollectAll.tsx
+++ hooks/useCollectAll.tsx
@@ -1,35 +1,35 @@
 import { useConnectModal } from "@rainbow-me/rainbowkit"
-import { useCollection } from "onchain-magic"
-import { useEthersSigner } from "../hooks/useEthersSigner"
-import useCheckNetwork from "../hooks/useCheckNetwork"
+import { useEthersSigner, useCollection } from "onchain-magic"
+import useCheckNetwork from "./useCheckNetwork"
 import { BASE_MINTER, CHAIN_ID, IS_TESTNET } from "../lib/consts"
 
 const useCollectAll = () => {
-    const signer = useEthersSigner()
-    const { openConnectModal } = useConnectModal()
-    const { checkNetwork } = useCheckNetwork()
-    const zoraDropAddress = process.env.NEXT_PUBLIC_DROP_ADDRESS
-    const { collectAll } = useCollection({
-      collectionAddress: zoraDropAddress,
-      chainId: CHAIN_ID,
-      minterOverride: IS_TESTNET ? "" : BASE_MINTER,
-    })
-  
-    const onClick = async () => {
-      if (!signer) {
-        openConnectModal()
-        return false
-      }
-      if (!checkNetwork()) {
-        return false
-      }
-      const response = await collectAll()
-      return response
-    }
+  const signer = useEthersSigner()
+  const { openConnectModal } = useConnectModal()
+  const { checkNetwork } = useCheckNetwork()
+  const zoraDropAddress = process.env.NEXT_PUBLIC_DROP_ADDRESS
+  const { collectAll, priceValues } = useCollection({
+    collectionAddress: zoraDropAddress,
+    chainId: CHAIN_ID,
+    minterOverride: IS_TESTNET ? "0x1Cd1C1f3b8B779B50Db23155F2Cb244FCcA06B21" : BASE_MINTER,
+  })
+  console.log("SWEETS priceValues", priceValues)
 
-    return {
-        onClick
+  const onClick = async () => {
+    if (!signer) {
+      openConnectModal()
+      return false
     }
+    if (!checkNetwork()) {
+      return false
+    }
+    const response = await collectAll()
+    return response
+  }
+
+  return {
+    onClick,
+  }
 }
 
 export default useCollectAll

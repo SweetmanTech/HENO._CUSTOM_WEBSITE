7b6186625b48c826e36d9aa3be9f2fa9cc25ab06
SeniorDev <tech.eng.me@gmail.com>
2024-04-16 06:55:59 -0400

    I add web3 title

--- components/Pages/Web3Page/Web3Content.tsx
+++ components/Pages/Web3Page/Web3Content.tsx
@@ -1,32 +1,31 @@
+import useIsMobile from "@/hooks/useIsMobile"
 import Trailer from "./Trailer"
 import RecBar from "../../RecBar"
-import useIsMobile from "../../../hooks/useIsMobile"
 import { useWeb3Drops } from "../../../providers/Web3Provider"
 import data from "../../../lib/zora-drops"
 
 const Web3Content = ({ isPopup = false }) => {
-  const isMobile = useIsMobile()
   const { selectedDrop } = useWeb3Drops()
+  const isMobile = useIsMobile()
 
   return (
     <div className="p-[5px] md:p-[10px] border-[2px] border-gray_1 h-full">
       <div
         className="border-[1px] border-darkgray text-gray_1 font-dresden 
             py-[25px] text-center
-            flex items-center justify-center text-[16px] relative h-full
+            flex flex-col md:flex-row
+            gap-2 items-center justify-center text-[16px] relative h-full
             text-[12px] md:text-[16px] relative"
       >
-        {!isMobile && (
-          <div
-            className={`absolute top-[35px] left-1 text-left md:left-2 capitalize ${
-              isPopup ? "text-[14px]" : "text-[16px]"
-            }`}
-          >
-            {data[selectedDrop].startedAt} <br />
-            <p className="uppercase">{data[selectedDrop].title}</p>
-            By {data[selectedDrop].artist}
-          </div>
-        )}
+        <div
+          className={`md:absolute top-[35px] left-1 text-left md:left-2 capitalize ${
+            isPopup ? "text-[10px] md:text-[14px]" : "text-[12px] md:text-[16px]"
+          }`}
+        >
+          {data[selectedDrop].startedAt} {!isMobile && <br />}
+          <span className="uppercase">{data[selectedDrop].title}</span> {isMobile && <br />}
+          By {data[selectedDrop].artist}
+        </div>
         <div
           className="h-fit max-h-full overflow-y-auto text-[12px] md:text-[16px]
             px-[15px] md:px-[20px] flex flex-col gap-y-[5px] md:gap-y-[20px]"

5b15911a6ea1b74a4aa38f8acef5c01c6ad17dc2
SeniorDev <tech.eng.me@gmail.com>
2024-04-17 14:43:02 -0400

    I make normal scroll

--- components/Pages/Web3Page/ZoraDropPlayers.tsx
+++ components/Pages/Web3Page/ZoraDropPlayers.tsx
@@ -4,21 +4,21 @@ import { useWeb3Drops } from "../../../providers/Web3Provider"
 
 const ZoraDropPlayers = ({ isPopup }) => {
   const { nextDrop, selectedDrop } = useWeb3Drops()
+
   return (
-    <div
-      className={`!overflow-y-auto
-       ${isPopup ? "!h-[200px]" : "!h-[300px]"}`}
-    >
+    <>
       {data.map((item, i) => (
         <VideoPlayer
           src={item.ipfs}
-          key={item.ipfs}
+          // eslint-disable-next-line react/no-array-index-key
+          key={i}
+          isActive={selectedDrop === i}
           nextDrop={nextDrop}
           selectedDrop={selectedDrop}
           isPopup={isPopup}
         />
       ))}
-    </div>
+    </>
   )
 }
 

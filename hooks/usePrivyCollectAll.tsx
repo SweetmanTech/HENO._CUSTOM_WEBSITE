6d53809329a1e29d82c2f8a9d4b85a2c7462bc96
SeniorDev <tech.eng.me@gmail.com>
2024-04-18 03:38:24 -0400

    I fix conflict

--- hooks/usePrivyCollectAll.tsx
+++ hooks/usePrivyCollectAll.tsx
@@ -1,25 +1,33 @@
-import { getCalldatas, useUniversalMinter } from "onchain-magic"
 import { BASE_MINTER, CHAIN_ID, IS_TESTNET, SEPOLIA_MINTER, ZORA_DROP_ADDRESS } from "@/lib/consts"
-import { BigNumber } from "ethers"
-import abi from "@/lib/abi/zora-UniversalMinter.json"
-import { toast } from "react-toastify"
 import handleTxError from "@/lib/handleTxError"
-import { useWeb3Drops } from "@/providers/Web3Provider"
 import usePreparePrivyWallet from "./usePreparePrivyWallet"
 import useConnectedWallet from "./useConnectedWallet"
 import usePrivySendTransaction from "./usePrivySendTransaction"
+import { useUserProvider } from "@/providers/UserProvider"
+import useWalletTransaction from "./useWalletTransaction"
+import { useState } from "react"
+import abi from "@/lib/abi/zora-drop.json"
+import { BigNumber } from "ethers"
+import { getCalldatas, useUniversalMinter } from "onchain-magic"
+import { useWeb3Drops } from "@/providers/Web3Provider"
 
-const usePrivyCollectAll = () => {
+const usePrivyCollect = () => {
   const { prepare } = usePreparePrivyWallet()
   const { connectedWallet } = useConnectedWallet()
   const { universalMinter } = useUniversalMinter(CHAIN_ID)
   const { drops, priceValues } = useWeb3Drops()
-  const { sendTransaction } = usePrivySendTransaction()
+  const { sendTransaction: sendTxByPrivy } = usePrivySendTransaction()
+  const { sendTransaction: sendTxByWallet } = useWalletTransaction()
+  const { isLoggedByEmail } = useUserProvider()
+  const [loading, setLoading] = useState(false)
 
-  const onClick = async () => {
+  const collect = async () => {
     try {
-      if (!prepare()) return
+      if (!(await prepare())) return false
+      if (!connectedWallet) return false
       if (!drops.length || !priceValues.length) return
+
+      setLoading(true)
       const targets = Array(drops.length).fill(ZORA_DROP_ADDRESS)
       const calldatas = getCalldatas(
         drops.length,
@@ -31,26 +39,42 @@ const usePrivyCollectAll = () => {
         (total: any, value: any) => total.add(BigNumber.from(value || "0")),
         BigNumber.from(0),
       )
-      await sendTransaction(
+
+      if (isLoggedByEmail) {
+        const response = await sendTxByPrivy(
+          universalMinter,
+          CHAIN_ID,
+          abi,
+          "mintBatchWithoutFees",
+          [targets, calldatas, priceValues],
+          totalValue.toHexString(),
+          "HENO.WEB3",
+          "COLLECT ALL",
+        )
+        setLoading(false)
+        return response
+      }
+
+      const response = await sendTxByWallet(
         universalMinter,
         CHAIN_ID,
         abi,
         "mintBatchWithoutFees",
         [targets, calldatas, priceValues],
-        totalValue.toHexString(),
-        "HENO.WEB3",
-        "COLLECT ALL",
+        totalValue.toHexString()
       )
 
-      toast.success("collected!")
+      setLoading(false)
+      return response
     } catch (error) {
       handleTxError(error)
     }
   }
 
   return {
-    onClick,
+    collect,
+    loading,
   }
 }
 
-export default usePrivyCollectAll
+export default usePrivyCollect

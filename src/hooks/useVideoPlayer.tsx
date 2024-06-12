fe3f6256f803bf0921dd6638d2424d3c515d762f
SeniorDev <tech.eng.me@gmail.com>
2024-06-12 14:15:46 -0400

    I fix warning

--- src/hooks/useVideoPlayer.tsx
+++ src/hooks/useVideoPlayer.tsx
@@ -4,19 +4,19 @@ const useVideoPlayer = (nextDrop, selectedDrop) => {
   const ref = useRef() as any
 
   useEffect(() => {
-    if (ref.current) {
-      const video = ref.current
+    const video = ref.current
+    if (video) {
       video.play()
       video.muted = true
       video.addEventListener("ended", nextDrop)
     }
 
     return () => {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      if (ref.current) ref.current.removeEventListener("ended", nextDrop)
+      if (video) {
+        video.removeEventListener("ended", nextDrop)
+      }
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedDrop, ref.current])
+  }, [selectedDrop, nextDrop])
 
   return {
     ref,

4a441393fc23576ea2e8465b71905efc8bd99c32
SeniorDev <tech.eng.me@gmail.com>
2024-08-06 09:31:04 -0400

    I fix feedback

--- src/components/Pages/Web3Page/Trailer.tsx
+++ src/components/Pages/Web3Page/Trailer.tsx
@@ -8,11 +8,11 @@ const Trailer = ({ isPopup = false }) => (
   <div className="h-full overflow-y-auto w-full">
     <div className={`flex flex-col items-center gap-y-[10px] ${!isPopup && "md:gap-y-[20px]"}`}>
       <div className="flex flex-col gap-y-[10px] h-full overflow-y-auto pr-2">
+        <HydroplanningSection isPopup={isPopup} />
         <TopArbitrumSection isPopup={isPopup} />
         <DropSection isPopup={isPopup} />
         <ArcadeSection isPopup={isPopup} />
         <NeyborsSection isPopup={isPopup} />
-        <HydroplanningSection isPopup={isPopup} />
       </div>
     </div>
   </div>
